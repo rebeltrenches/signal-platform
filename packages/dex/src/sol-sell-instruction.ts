dfc73bd3326a0849deb4910b7457fb1f658f6c0f
rebeltrenches <gift.nagel@gmail.com>
2026-09-22 00:04:06 +0200

    Bind sell settlement instruction to unique trade ID

--- packages/dex/src/sol-sell-instruction.ts
+++ packages/dex/src/sol-sell-instruction.ts
@@ -1,6 +1,6 @@
 import { PublicKey, TransactionInstruction } from '@solana/web3.js';
 import { TOKEN_PROGRAM_ID } from '@solana/spl-token';
-import { WRAPPED_SOL_MINT, deriveSolSellSettlementAccounts } from './sol-sell-accounts.js';
+import { WRAPPED_SOL_MINT, deriveSolSellSettlementAccounts, parseTradeId } from './sol-sell-accounts.js';
 
 export const SETTLE_SELL_INSTRUCTION = 1;
 
@@ -8,9 +8,11 @@ export function buildSolSellSettlementInstruction(params: {
   programId: string;
   creatorAddress: string;
   traderAddress: string;
+  tradeId: string;
 }): TransactionInstruction {
   const programId = new PublicKey(params.programId);
   const accounts = deriveSolSellSettlementAccounts(params);
+  const tradeId = parseTradeId(params.tradeId);
 
   return new TransactionInstruction({
     programId,
@@ -24,6 +26,6 @@ export function buildSolSellSettlementInstruction(params: {
       { pubkey: WRAPPED_SOL_MINT, isSigner: false, isWritable: false },
       { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },
     ],
-    data: Buffer.from([SETTLE_SELL_INSTRUCTION]),
+    data: Buffer.concat([Buffer.from([SETTLE_SELL_INSTRUCTION]), tradeId]),
   });
 }
